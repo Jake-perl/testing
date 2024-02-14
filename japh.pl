02f1f0bf4f4c03dc0b5f7804fe838c057e710dff
Arne Becker <arne@ebi.ac.uk>
2024-02-14 14:33:44 +0000

    Repo needs more japh!

---
+++ japh.pl
@@ -0,0 +1,4 @@
+$_="b1r11bh1b_s0b11ub0e0bbh111t0b_k_b0J__a1b1P_b1n1bbl1_ 0b10c11_o";s$b
+$01$gx;s$_$00$g;$b={(split/(\d+)/)[1..32]};$_=unpack"B*",q,"'4> #99*.,.
+'+):94!1",';s,...(.{5}),$1,g;for(split//){$a.=$_;$/=$b->{$a};$/?($,.=$/
+,$a=""):1}; print "" , "\n"; # Arne Becker - Just another Perl hacker
