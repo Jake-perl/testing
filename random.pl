82633408947a7f565b52546551aa4b7b4328aafd
Jake-perl <159278555+Jake-perl@users.noreply.github.com>
2024-02-14 20:45:59 +0000

    I love Perl

--- random.pl
+++ random.pl
@@ -13,3 +13,40 @@ print "hello Github\n",
 my $variable = 'Arne';
 
 my ($age, $name) = (13, 'Jake')
+
+my $the_truth = "$variable is better at perl than \"$name\"\n";
+
+$the_truth = qq{$variable is better at perl than"$name"\n};
+
+print $the_truth;
+
+my $bunch_of_info = <<"END";
+This is a
+bunch of information
+on multiple lines
+END
+
+say $bunch_of_info;
+
+my $big_int = 18446744073709551615
+
+# c : Character
+# s : string
+# d : Decimal
+# u : Unsigned integer
+# f : Floating point (Decimal Notation)
+# e : Floating point (Scientific Notation)
+
+printf(%u \n", $big_int + 1);
+
+my $big_float = .1000000000000001;
+
+printf("%.16f \n" $big_float)
+
+my $first = 1;
+
+my $second = 2;
+
+
+
+
