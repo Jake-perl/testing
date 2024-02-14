75303361f104186b8caf10a611c2674d3751af83
Jake-perl <159278555+Jake-perl@users.noreply.github.com>
2024-02-14 21:02:40 +0000

    Update HowToPerlTutorial.pl

--- Perl Tutorials/HowToPerlTutorial.pl
+++ Perl Tutorials/HowToPerlTutorial.pl
@@ -1,1 +1,8 @@
-
+$ perl -e 'print "hello\n"'
+# how to print something
+$ perl -E 'say "hello\n"'
+# creates a fresh, new line after saying hello
+$ perl -E 'say "hello"'
+# Does not create the blank line 
+$ perl -E 'while(<>) {say uc $_};
+# loop
