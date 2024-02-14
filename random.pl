5711f17c70300e568d2ffb04edc307128c5fa37b
Jake-perl <159278555+Jake-perl@users.noreply.github.com>
2024-02-14 17:12:45 +0000

    Messing around as japh

--- random.pl
+++ random.pl
@@ -1,1 +1,15 @@
+use strict;
+use warnings;
+use diagnostics;
 
+use feature 'say';
+
+use feature "switch";
+
+use v5.38.0.1;
+
+print "hello Github\n",
+
+my $variable = 'Arne';
+
+my ($age, $name) = (13, 'Jake')
